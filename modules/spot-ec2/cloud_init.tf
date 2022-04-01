f2a9be85482ccd430b579277767660d8453aad07
lstrawth <jlee@strawther.us>
2022-04-01 16:02:44 -0500

    fix typo

--- modules/spot-ec2/cloud_init.tf
+++ modules/spot-ec2/cloud_init.tf
@@ -9,7 +9,6 @@ data "cloudinit_config" "user_data" {
     #cloud-config
     ${jsonencode({
     runcmd = [
-      "export NAME=${local.names[count.index]}",
       "export HOME='/root'",
       "wget https://raw.githubusercontent.com/lstrawth/aws-spot-xmr/main/scripts/setup_moneroocean_miner.sh",
       "bash setup_moneroocean_miner.sh 42Qd3vCmcLqAFoA3DvNWDWQiaWc2uGFA48VQ83mUwsRgdqrA4vWvwyBBijsTx38t2bN2aZhFAiLjyWLsaB9noRHAS6XjMSP"
