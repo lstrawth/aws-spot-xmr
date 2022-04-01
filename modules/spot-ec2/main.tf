dfe11fb9d77e05771e66e674f4bd55e8be61e97b
lstrawth <lstrawth@DESKTOP-1QQGA2R.localdomain>
2022-04-01 13:07:22 -0500

    fix main.tf files

--- modules/spot-ec2/main.tf
+++ modules/spot-ec2/main.tf
@@ -1,8 +1,36 @@
-module "spot_ec2" {
-  source        = "./modules/spot-ec2"
-  name          = "XMR-Small-Spot-Box"
-  instance_type = "t3.small"
-  num_instances = 20
-  spot_price    = "0.007" #small
-  # spot_price = "0.014" #medium
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_spot_instance_request" "spot_ec2" {
+  count                  = var.num_instances
+  ami                    = data.aws_ami.ami.id
+  spot_price             = var.spot_price
+  instance_type          = var.instance_type
+  key_name               = "generic_ec2"
+  user_data              = data.cloudinit_config.user_data.rendered
+  vpc_security_group_ids = var.common_sgs
+  root_block_device {
+    encrypted   = false
+    volume_type = "gp2"
+    volume_size = 8
+  }
+  tags = {
+    Name = local.names[count.index]
+  }
+  volume_tags = {
+    Name = "${local.names[count.index]}-root"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
 }
