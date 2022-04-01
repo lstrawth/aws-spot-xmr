b8df0cdfec7d87ca069865f1aefda6f878c095ef
lstrawth <lstrawth@DESKTOP-1QQGA2R.localdomain>
2022-04-01 13:50:38 -0500

    change instances from 20 to 25

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "spot_ec2" {
   source        = "./modules/spot-ec2"
   name          = "XMR-Small-Spot-Box"
   instance_type = "t3.small"
-  num_instances = 20
+  num_instances = 25
   spot_price    = "0.007" #small
   # spot_price = "0.014" #medium
 }
